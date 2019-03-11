203017a07d44bf9f9fa49d1abc38d2c06f646bde
joyalwaysin <joyalwaysin@gmail.com>
2019-03-11 18:56:26 +0530

    .

--- app/src/main/java/com/nagainfo/mobiremit/activities/LandingActivity.java
+++ app/src/main/java/com/nagainfo/mobiremit/activities/LandingActivity.java
@@ -39,6 +39,7 @@ public class LandingActivity extends AppCompatActivity implements View.OnClickLi
             Manifest.permission.READ_EXTERNAL_STORAGE,
             Manifest.permission.WRITE_EXTERNAL_STORAGE
     };
+    
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
